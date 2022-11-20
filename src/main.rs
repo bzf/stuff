d7c0b2c3120ba0effe134dfeb00ed47cca4ee1ed
André Ligné <andre@ligne.io>
2022-11-20 14:54:55 +0100

    Extract separate workspace for the core stuff

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,9 @@
 
 use clap::{Parser, Subcommand};
 use itertools::Itertools;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process::exit;
 
-use crate::config::Config;
-use crate::store::Store;
-
-mod config;
-mod project;
-mod store;
-mod task;
-
-const CONFIG_FILENAME: &'static str = "config.yml";
+use stuff::{get_config, initialize_configuration, Store};
 
 #[derive(Debug, Parser)]
 #[command(name = "stuff")]
@@ -71,21 +61,7 @@ fn main() {
             }
 
             None => {
-                let data_directory = xdg_dirs.get_data_home();
-
-                let initial_configuration = Config::new(data_directory.as_path());
-
-                let filepath = xdg_dirs
-                    .place_config_file(CONFIG_FILENAME)
-                    .expect("Failed to create config directory");
-
-                let mut file = File::create(filepath).unwrap();
-                file.write_all(
-                    serde_yaml::to_string(&initial_configuration)
-                        .unwrap()
-                        .as_bytes(),
-                )
-                .unwrap();
+                initialize_configuration(&xdg_dirs);
             }
         },
 
@@ -130,13 +106,6 @@ fn main() {
             exit(1);
         }
     }
-}
-
-fn get_config(xdg_dirs: &xdg::BaseDirectories) -> Option<Config> {
-    xdg_dirs
-        .find_config_file(CONFIG_FILENAME)
-        .and_then(|filepath| File::open(filepath).ok())
-        .and_then(|file| serde_yaml::from_reader(file).ok())
 }
 
 fn concat_parts(parts: Vec<String>) -> String {
