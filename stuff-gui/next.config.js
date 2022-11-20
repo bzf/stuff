586f9b2666ef78f2ef70f06af6944b7a0e134cc3
André Ligné <andre@ligne.io>
2022-11-20 18:22:33 +0100

    Redirect / to /inbox when starting the app

--- stuff-gui/next.config.js
+++ stuff-gui/next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/inbox",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
