febff84f67056bc358a3a60d46ba7fac34b25785
André Ligné <andre@ligne.io>
2022-12-10 17:18:22 +0100

    Show tasks on the Today view

--- stuff-gui/src/pages/today.tsx
+++ stuff-gui/src/pages/today.tsx
@@ -1,13 +1,68 @@
+import { moveTaskToPosition, useTasks } from "../stuff";
 import PageTitle from "../components/PageTitle";
+import NewTaskForm from "../components/NewTaskForm";
+import TaskItem from "../components/TaskItem";
+import TaskForm from "../components/TaskForm";
+import useEditTask from "../hooks/useEditTask";
+import { ReactSortable } from "react-sortablejs";
+import dayjs from "dayjs";
 
-function App() {
+export default function Today() {
+  const tasks = useTasks();
+  const { save, cancel, editTask, setEditTask } = useEditTask();
+
+  if (tasks === undefined) {
+    return <div>loading</div>;
+  }
+
+  function handleTaskMove(event) {
+    const { newIndex, item } = event;
+    const { taskId } = item.dataset;
+
+    moveTaskToPosition(taskId, newIndex);
+  }
+
+  const visibleTasks = tasks
+    .filter((task) => !task.projectId && !task.areaId)
+    .filter((task) => task.deferDate)
+    .filter((task) => dayjs(task.deferDate).isBefore(new Date(), "day"));
+
   return (
     <>
-      <div className="flex flex-col pb-4 px-3">
-        <PageTitle title="Today" />
+      <div>
+        <div className="flex flex-col pb-4 px-3">
+          <PageTitle title="Today" />
+
+          <ReactSortable
+            group="groupName"
+            animation={200}
+            delay={2}
+            list={visibleTasks}
+            setList={() => null}
+            onEnd={handleTaskMove}
+          >
+            {visibleTasks.map((task) =>
+              task.id === editTask?.id ? (
+                <TaskForm
+                  key={task.id}
+                  initialNote={editTask.description}
+                  initialTitle={editTask.title}
+                  initialDeferDate={editTask.deferDate}
+                  onUpdate={save}
+                  onCancel={cancel}
+                />
+              ) : (
+                <TaskItem
+                  task={task}
+                  key={task.id}
+                  onDoubleClick={() => setEditTask(task)}
+                />
+              )
+            )}
+          </ReactSortable>
+        </div>
+        <NewTaskForm projectId={undefined} />
       </div>
     </>
   );
 }
-
-export default App;
