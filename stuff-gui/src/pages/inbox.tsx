aaeb205d0490727b28d353177ce1bcbe25037c2a
André Ligné <andre@ligne.io>
2022-12-10 22:53:08 +0100

    Remove drag and drop for the Inbox and Today views

--- stuff-gui/src/pages/inbox.tsx
+++ stuff-gui/src/pages/inbox.tsx
@@ -1,10 +1,9 @@
-import { moveTaskToPosition, useTasks } from "../stuff";
+import { useTasks } from "../stuff";
 import PageTitle from "../components/PageTitle";
 import NewTaskForm from "../components/NewTaskForm";
 import TaskItem from "../components/TaskItem";
 import TaskForm from "../components/TaskForm";
 import useEditTask from "../hooks/useEditTask";
-import { ReactSortable } from "react-sortablejs";
 
 function App() {
   const tasks = useTasks();
@@ -12,13 +11,6 @@ function App() {
 
   if (tasks === undefined) {
     return <div>loading</div>;
-  }
-
-  function handleTaskMove(event) {
-    const { newIndex, item } = event;
-    const { taskId } = item.dataset;
-
-    moveTaskToPosition(taskId, newIndex);
   }
 
   const visibleTasks = tasks
@@ -31,34 +23,26 @@ function App() {
         <div className="flex flex-col pb-4 px-3">
           <PageTitle title="Inbox" />
 
-          <ReactSortable
-            group="groupName"
-            animation={200}
-            delay={2}
-            list={visibleTasks}
-            setList={() => null}
-            onEnd={handleTaskMove}
-          >
-            {visibleTasks.map((task) =>
-              task.id === editTask?.id ? (
-                <TaskForm
-                  key={task.id}
-                  initialNote={editTask.description}
-                  initialTitle={editTask.title}
-                  initialDeferDate={editTask.deferDate}
-                  onUpdate={save}
-                  onCancel={cancel}
-                />
-              ) : (
-                <TaskItem
-                  task={task}
-                  key={task.id}
-                  onDoubleClick={() => setEditTask(task)}
-                />
-              )
-            )}
-          </ReactSortable>
+          {visibleTasks.map((task) =>
+            task.id === editTask?.id ? (
+              <TaskForm
+                key={task.id}
+                initialNote={editTask.description}
+                initialTitle={editTask.title}
+                initialDeferDate={editTask.deferDate}
+                onUpdate={save}
+                onCancel={cancel}
+              />
+            ) : (
+              <TaskItem
+                task={task}
+                key={task.id}
+                onDoubleClick={() => setEditTask(task)}
+              />
+            )
+          )}
         </div>
+
         <NewTaskForm projectId={undefined} />
       </div>
     </>
--- stuff-gui/src/pages/today.tsx
+++ stuff-gui/src/pages/today.tsx
@@ -1,10 +1,9 @@
-import { moveTaskToPosition, useTasks } from "../stuff";
+import { useTasks } from "../stuff";
 import PageTitle from "../components/PageTitle";
 import NewTaskForm from "../components/NewTaskForm";
 import TaskItem from "../components/TaskItem";
 import TaskForm from "../components/TaskForm";
 import useEditTask from "../hooks/useEditTask";
-import { ReactSortable } from "react-sortablejs";
 import dayjs from "dayjs";
 
 export default function Today() {
@@ -13,13 +12,6 @@ export default function Today() {
 
   if (tasks === undefined) {
     return <div>loading</div>;
-  }
-
-  function handleTaskMove(event) {
-    const { newIndex, item } = event;
-    const { taskId } = item.dataset;
-
-    moveTaskToPosition(taskId, newIndex);
   }
 
   const visibleTasks = tasks
@@ -33,34 +25,26 @@ export default function Today() {
         <div className="flex flex-col pb-4 px-3">
           <PageTitle title="Today" />
 
-          <ReactSortable
-            group="groupName"
-            animation={200}
-            delay={2}
-            list={visibleTasks}
-            setList={() => null}
-            onEnd={handleTaskMove}
-          >
-            {visibleTasks.map((task) =>
-              task.id === editTask?.id ? (
-                <TaskForm
-                  key={task.id}
-                  initialNote={editTask.description}
-                  initialTitle={editTask.title}
-                  initialDeferDate={editTask.deferDate}
-                  onUpdate={save}
-                  onCancel={cancel}
-                />
-              ) : (
-                <TaskItem
-                  task={task}
-                  key={task.id}
-                  onDoubleClick={() => setEditTask(task)}
-                />
-              )
-            )}
-          </ReactSortable>
+          {visibleTasks.map((task) =>
+            task.id === editTask?.id ? (
+              <TaskForm
+                key={task.id}
+                initialNote={editTask.description}
+                initialTitle={editTask.title}
+                initialDeferDate={editTask.deferDate}
+                onUpdate={save}
+                onCancel={cancel}
+              />
+            ) : (
+              <TaskItem
+                task={task}
+                key={task.id}
+                onDoubleClick={() => setEditTask(task)}
+              />
+            )
+          )}
         </div>
+
         <NewTaskForm projectId={undefined} />
       </div>
     </>
